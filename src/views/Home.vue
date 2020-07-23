d6c79f08fab9b101c37e3eb29b61401cb005a768
Alexander Galea <agalea91@gmail.com>
2020-07-22 22:26:37 -0700

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <HomePage testing="false"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + Flask App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from '@/components/HomePage.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    HomePage
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
