0ebc81648c64f2d5757343264d8c1f1a5dd44eb9
Alexander Galea <agalea91@gmail.com>
2020-12-05 20:05:31 -0800

    Fix header

--- eb-app/api/__init__.py
+++ eb-app/api/__init__.py
@@ -16,7 +16,7 @@ api_rest = Api(api_bp)
 @api_bp.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    response.headers['Access-Control-Allow-Origin'] = 'https://ravenslight.com'
+    response.headers['Access-Control-Allow-Origin'] = 'https://ravenslightphoto.com'
     response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,POST,GET'
     return response
 
