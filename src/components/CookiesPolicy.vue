3f29a50f068e3948ab7cdabdd68e642bc58399c4
Alexander Galea <agalea91@gmail.com>
2021-01-18 01:02:13 -0500

    Add gdpr pages

---
+++ src/components/CookiesPolicy.vue
@@ -0,0 +1,103 @@
+<template>
+
+  <div id="app">
+    <h1>Cookies Policy</h1>
+    <div class="main-content">
+      <p>
+        Last updated: January 18, 2021
+        <br>
+        <br>
+        This Cookies Policy explains what Cookies are and how We use them. You should read this policy so You can understand what type of cookies We use, or the information We collect using Cookies and how that information is used. This Cookies Policy has been created with the help of the Cookies Policy Generator.
+        <br>
+        <br>
+        Cookies do not typically contain any information that personally identifies a user, but personal information that we store about You may be linked to the information stored in and obtained from Cookies. For further information on how We use, store and keep your personal data secure, see our Privacy Policy.
+        <br>
+        <br>
+        We do not store sensitive personal information, such as mailing addresses, account passwords, etc. in the Cookies We use.
+      </p>
+      <p>
+
+      </p>
+      <h2>Definitions</h2>
+      <p>
+        For the purposes of this Cookies Policy:
+        <br>
+        <br>
+        Company (referred to as either "the Company", "We", "Us" or "Our" in this Cookies Policy) refers to Raven's Light Photo Journal.
+        Cookies means small files that are placed on Your computer, mobile device or any other device by a website, containing details of your browsing history on that website among its many uses.
+        Website refers to Raven's Light Photo Journal, accessible from https://ravenslightphoto.com/
+        You means the individual accessing or using the Website, or a company, or any legal entity on behalf of which such individual is accessing or using the Website, as applicable.
+      </p>
+      <h2>The use of the Cookies</h2>
+      <h3>Type of Cookies We Use</h3>
+      <p>
+        Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies remain on your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close your web browser.
+        <br>
+        <br>
+        Session and persistent Cookies may be used to remember choices you've made on the website. Additional necessary cookies are Google Ads and Analytics cookies. These help us provide relevant advertising and improve the website. The cookies collect information in a way that does not directly identify anyone.
+      </p>
+      <h3>Your Choices Regarding Cookies</h3>
+      <p>
+        If You prefer to avoid the use of Cookies on the Website, first You must disable the use of Cookies in your browser and then delete the Cookies saved in your browser associated with this website. You may use this option for preventing the use of Cookies at any time.
+        <br>
+        <br>
+        If You do not accept Our Cookies, You may experience some inconvenience in your use of the Website and some features may not function properly.
+        <br>
+        <br>
+        If You'd like to delete Cookies or instruct your web browser to delete or refuse Cookies, please visit the help pages of your web browser.
+      </p>
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about this Cookies Policy, You can contact us by visiting this page on the website: https://ravenslightphoto.com/about
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CookiesPolicy',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.main-content {
+  font-family: 'Playfair Display', serif;
+  font-weight:normal;
+  display: inline-block;
+  width: 600px;
+  max-width: 90%;
+
+  margin: 5px 0 0;
+  text-align: left;
+  padding-left: 10px;
+}
+// ul {
+//   list-style-type: none;
+//   padding: 0;
+// }
+// li {
+//   // display: inline-block;
+//   margin: 30px;
+// }
+a {
+  color: #42b983;
+}
+.post-summary {
+  width: 1000px;
+  max-width: 100%;
+  margin-left:auto;
+  margin-right:auto;
+  line-height: 100%;
+}
+.post-cover-image {
+  max-width: 100%;
+  margin: 10px;
+}
+.headline-below-h1 {
+  padding: 20px
+}
+</style>
---
+++ src/components/GdprAlert.vue
@@ -0,0 +1,59 @@
+<template>
+  <transition name="fade">
+    <div class="modal" v-if="show">
+      <div id="gdpr-alert">
+        By browsing this site you consent to our <a href="/cookies-policy">Cookies Policy</a>
+        <button type="button" class="modal-button" @click="closeModal()">
+          <svg class="modal-x-svg" xmlns="http://www.w3.org/2000/svg" viewBox="-25 -25 80 80" width="50" overflow="visible" stroke="black" stroke-width="10" stroke-linecap="round">
+            <line x1="0" y1="0" x2="50" y2="50" />
+            <line x1="50" y1="0" x2="0" y2="50" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'GdprAlert',
+  data () {
+    return {
+      show: true
+    }
+  },
+  methods: {
+    closeModal () {
+      this.show = false
+      document.querySelector('body').classList.remove('overflow-hidden')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.modal-button {
+  border: 0px;
+  background: white;
+  :hover {
+    cursor: pointer;
+  }
+}
+
+.modal-x-svg {
+  width: 10px;
+  height: auto;
+}
+
+</style>
