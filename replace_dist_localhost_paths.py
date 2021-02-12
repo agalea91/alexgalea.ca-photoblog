dcb8f7f171bf28ac6668298a8650d62275bb4f5d
Alexander Galea <agalea91@gmail.com>
2021-02-12 13:06:11 -0500

    Replace api host

--- replace_dist_localhost_paths.py
+++ replace_dist_localhost_paths.py
@@ -9,9 +9,15 @@ def main():
     html_files = glob.glob(f"{DIST_FP}/**/*.html", recursive=True)
 
     for fp in html_files:
-        replace_localhost(fp)
+        replace_localhost_client(fp)
 
-def replace_localhost(fp):
+    print("Replacing 'http://localhost:5000' with 'https://api.ravenslight.com' for *.js files")
+    js_files = glob.glob(f"{DIST_FP}/static/js/app.*.js")
+    replace_localhost_server(js_files[0])
+
+
+
+def replace_localhost_client(fp):
     with open(fp, "r") as f:
         html = f.read()
 
@@ -19,6 +25,13 @@ def replace_localhost(fp):
     with open(fp, "w") as f:
         f.write(html_)
 
+def replace_localhost_server(fp):
+    with open(fp, "r") as f:
+        js = f.read()
+
+    js_ = re.sub(r'http\://localhost\:5000', 'https://api.ravenslightphoto.com', js)
+    with open(fp, "w") as f:
+        f.write(js_)
 
 if __name__ == '__main__':
     main()
