cea9833521b6579bd422b08d9b5f60cdafc69c1c
Alexander Galea <agalea91@gmail.com>
2021-06-18 15:09:58 -0400

    Update copy

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,14 +4,6 @@
     <h1>About</h1>
     <div class="main-content">
       <p>
-        I've been shooting Canon SLRs since 2005. My lens setup:
-      </p>
-      <ul>
-        <li>70-200mm f/4.0</li>
-        <li>60mm f/2.8 macro</li>
-        <li>17-50mm f/2.8</li>
-      </ul>
-      <p>
         Living in Western Canada has given me the opportunity to photograph amazing scenery.
         <br><br>
         I've become fascinated by the stories of the native peoples of the Pacific North-West.
@@ -21,10 +13,35 @@
       </p>
       <h2>Licensing</h2>
       <p>
-        Thanks for your interest! Please contact alex@alexgalea.ca for
-        licensing inquiries. My per-image liscence costs are comparable to
+        Download the high resolution JPG file for any photograph on the
+        site. Watermark removed and full resolution (up to 4000px).
+      </p>
+      <ul>
+        <li><a href="https://www.buymeacoffee.com/alexgalea/e/34863">PHOTO - Full Quality Digital Download ($20)</a></li>
+        <li><a href="https://www.buymeacoffee.com/alexgalea/e/34864">ALBUM - Full Quality Digital Download ($50)</a></li>
+      </ul>
+      <p>
+        10% of sales go to Indspire - a Canadian Indigenous registered
+        charity that invests in the education of First Nations.
+      </p>
+      <p>
+        CC BY-NC 3.0 for non-commercial use.
+      </p>
+      <h2>Commercial Licensing</h2>
+      <p>
+        Please contact alex@alexgalea.ca for commercial licensing. My
+        per-image liscence costs are comparable to
         <a href="https://www.shutterstock.com/license-comparison">Shutterstock</a>.
       </p>
+      <h2>Camera Specs</h2>
+      <p>
+        I've been shooting Canon SLRs since 2005. My lens setup:
+      </p>
+      <ul>
+        <li>70-200mm f/4.0</li>
+        <li>60mm f/2.8 macro</li>
+        <li>17-50mm f/2.8</li>
+      </ul>
     </div>
     <div id="buyme-coffee">
       <a href="https://www.buymeacoffee.com/alexgalea"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=alexgalea&button_colour=000000&font_colour=ffffff&font_family=Cookie&outline_colour=ffffff&coffee_colour=FFDD00"></a>
