d6de6b2b71d6fd9e369e73f7dc8264f7be0a7f6d
Alexander Galea <agalea91@gmail.com>
2021-06-23 00:54:31 -0400

    Add processed folder for IG uploads, improved single post update

--- update_public_posts.py
+++ update_public_posts.py
@@ -14,6 +14,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+PROC_PATH = pathlib.Path("processed")
 POST_PATH = pathlib.Path("posts")
 PUB_PATH = pathlib.Path("public")
 IGNORE_POSTS_STARTSWITH = "_"
@@ -22,23 +23,21 @@ MAX_PX_SIZE = 1280
 WATERMARK_PHOTOGRAPHER = "Alexander Galea"
 WATERMARK_DOMAIN = "ravenslightphoto.com"
 
-def main():
-    args = parse_args()
-    post_slug = args.post_slug
+def main(post_slug):
+    """ Process images in "posts" - shrink, watermark, etc.
+    
+    Default is to process all posts unless post_slug argument is given.
+
+    post_slug : str
+        Only process this post.
+    """
     if post_slug:
         print(f"Copying single post: {post_slug}")
+        print("Desination folder is not cleaned prior to processing.")
     else:
         print("Refreshing all posts")
+        delete_folder(PUB_PATH / "img")
 
-    pub_img_fp = PUB_PATH / "img"
-    ans = input(f"WARNING: about to delete {str(pub_img_fp)} folder. Press enter to proceed.")
-    if ans.strip() == "":
-        try:
-            shutil.rmtree(pub_img_fp)
-        except FileNotFoundError:
-           pass
-    else:
-        sys.exit("User exit")
 
     post_cat_img_fp = POST_PATH / "img" / "categories"
     pub_cat_img_fp = PUB_PATH / "img" / "categories"
@@ -70,6 +69,10 @@ def main():
             image_files = []
 
         pub_fp = PUB_PATH / (pathlib.Path(*fp.parts[1:]))
+        proc_fp = PROC_PATH / (pathlib.Path(*fp.parts[1:]))
+        if post_slug:
+            delete_folder(pub_fp)
+            delete_folder(proc_fp)
 
         post_fp = (fp / "post.json")
         pub_post_fp = (pub_fp / "post.json")
@@ -79,31 +82,39 @@ def main():
 
         for image in image_files:
             image_fp = (fp / image["file"])
+            proc_image_fp = dict(
+                small=(proc_fp / "processed" / f"sm_{image['file']}"),
+                small_watermark=(proc_fp / "processed" / f"sm_wk_{image['file']}"),
+            )
             pub_image_fp = (pub_fp / image["file"])
             print(image_fp)
 
             image_obj = load_image(image_fp)
-            image_obj = resize_pixels(image_obj, MAX_PX_SIZE)
-            image_obj = add_watermark(image_obj, date=image["date"])
+            image_obj_small = resize_pixels(image_obj, MAX_PX_SIZE)
+            image_obj_small_watermark = add_watermark(image_obj_small, date=image["date"])
 
+            # Write to "processed" folder
+            print(f"{str(image_fp)} -> {str(proc_image_fp['small'])}")
+            write_image(image_obj_small, proc_image_fp['small'])
+            print(f"{str(image_fp)} -> {str(proc_image_fp['small_watermark'])}")
+            write_image(image_obj_small_watermark, proc_image_fp['small_watermark'])
+
+            # Write to "public" folder
             print(f"{str(image_fp)} -> {str(pub_image_fp)}")
-            write_image(image_obj, pub_image_fp)
+            write_image(image_obj_small_watermark, pub_image_fp)
 
 
+def delete_folder(folder):
+    ans = input(f"WARNING: about to delete {str(folder)} folder. Press enter to proceed.")
+    if ans.strip() == "":
+        try:
+            shutil.rmtree(folder)
+        except FileNotFoundError:
+           pass
+    else:
+        sys.exit("User exit")
 
-def parse_args():
-    import argparse
-    parser = argparse.ArgumentParser(description="Convert images in posts to public images for website")
-    parser.add_argument(
-        "--post-slug",
-        default="",
-        help=(
-            "Update a single post (skip the rest). "
-            "Example slug: 4-lakota-dream-of-what-was-to-be"
-        )
-    )
-    args = parser.parse_args()
-    return args
+
 
 
 def load_image(image_fp):
@@ -176,10 +187,23 @@ def read_post_images(post_fp):
 
 
 def write_image(image_obj, pub_image_fp):
+    pub_image_fp.parent.mkdir(exist_ok=True, parents=True)
     image_obj.convert("RGB").save(pub_image_fp, optimize=True, quality=95)
     print(f"{ pathlib.Path(pub_image_fp).stat().st_size / 1e3 } kb")
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description="Convert images in posts to public images for website")
+    parser.add_argument(
+        "--post-slug",
+        default="",
+        help=(
+            "Update a single post (skip the rest). "
+            "Example slug: 4-lakota-dream-of-what-was-to-be"
+        )
+    )
+    args = parser.parse_args()
+    post_slug = args.post_slug
+    main(post_slug)
 
